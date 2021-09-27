30defba5aae082225e7afa60186f25ec6f88c4eb
Nick <nicholasg1989@gmail.com>
2021-09-26 22:01:15 -0700

    fix filename

--- updater.sh
+++ updater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 get_update.py && \
-sudo dpkg -i plexmediaserver.deb && \
+sudo dpkg -i plex_update.deb && \
 sudo apt-get install -f && \
-rm -rf plexmediaserver.deb
+rm -rf plex_update.deb
